abb6f8d5a3af3095dfc702e88493d20998eceebd
DaisukeSato56 <workshift56@gmail.com>
2020-07-04 16:37:32 +0900

    modify settings.rb

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ def main
   settings = Settings.new
   puts settings.account_id
   puts settings.account_token
+  puts settings.trade_num_ranking
 end
 
 main if __FILE__ == $PROGRAM_NAME
--- settings.rb
+++ settings.rb
@@ -3,11 +3,24 @@
 require 'yaml'
 
 class Settings
-  attr_accessor :account_id, :account_token
+  attr_accessor :account_id, :account_token, :account_product_code, :db_name, :db_driver, :web_port, :trade_duration, :trade_back_test, :trade_use_percent, :trade_past_period, :trade_stop_limit_percent, :trade_num_ranking
 
   def initialize
-    conf = YAML.load_file('setting.yml')
+    conf = YAML.load_file('settings.yml')
     @account_id = conf['oanda']['account_id']
     @account_token = conf['oanda']['access_token']
+    @account_product_code = conf['oanda']['product_code']
+
+    @db_name = conf['db']['name']
+    @db_driver = conf['db']['driver']
+
+    @web_port = conf['web']['port']
+
+    @trade_duration = conf['rbtrading']['trade_duration'].downcase
+    @trade_back_test = conf['rbtrading']['back_test']
+    @trade_use_percent = conf['rbtrading']['use_percent']
+    @trade_past_period = conf['rbtrading']['past_period']
+    @trade_stop_limit_percent = conf['rbtrading']['stop_limit_percent']
+    @trade_num_ranking = conf['rbtrading']['num_ranking']
   end
 end
